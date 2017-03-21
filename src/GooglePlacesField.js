96a654f60c375060e9d0003eafcd83aa547537e2
Chris Heninger <chrish@synapsestudios.com>
2017-03-20 19:44:15 -0700

    Updates for 0.3.0 bump and proposal for renaming– docs/example WIP

--- src/GooglePlacesField.js
+++ src/GooglePlacesField.js
@@ -1,29 +1,20 @@
-/* eslint import/no-unresolved: 0 */
-/* eslint import/extensions: 0 */
 import React, { Component, PropTypes } from 'react';
 import Geosuggest from 'react-geosuggest';
 import omit from 'lodash.omit';
-import './style.css';
 
 class GooglePlacesField extends Component {
   static propTypes = {
     id: PropTypes.string,
     onSelect: PropTypes.func.isRequired,
     placeholder: PropTypes.string,
-  }
+  };
 
   static defaultProps = {
     id: 'google-places-field',
     placeholder: 'Start Typing',
-  }
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.onSelect = this.onSelect.bind(this);
-  }
-
-  onSelect(data) {
+  onSelect = data => {
     if (!data.gmaps) {
       return;
     }
@@ -31,7 +22,7 @@ class GooglePlacesField extends Component {
     const formattedData = {};
     let streetNumber = '';
     let route = '';
-    data.gmaps.address_components.forEach((addressPart) => {
+    data.gmaps.address_components.forEach(addressPart => {
       if (addressPart.types.indexOf('street_number') !== -1) {
         streetNumber = addressPart.long_name;
       }
@@ -54,13 +45,15 @@ class GooglePlacesField extends Component {
     formattedData.data = data;
 
     this.props.onSelect(formattedData);
-  }
+  };
 
   render() {
     const passThruProps = omit(this.props, ['id', 'onSelect', 'placeholder']);
     return (
       <Geosuggest
-        ref={(input) => { this.geosuggestInputRef = input; }}
+        ref={input => {
+          this.geosuggestInputRef = input;
+        }}
         id={this.props.id}
         onSuggestSelect={this.onSelect}
         placeholder={this.props.placeholder}
---
+++ storybook/stories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
+import '../../styles/index.css';
+
+storiesOf(
+  '<ReactGeosuggest />',
+  module,
+).add('setState Example (default)', () => (
+  <div>
+    <h1>react-google-places</h1>
+    <h3>Example Usage:</h3>
+    <pre>
+      {`import React, { Component } from 'react';`}
+    </pre>
+  </div>
+));
